95ea9112f24552815e2cd70c459acf4babc3ffc0
averync2005 <109486673+averync2005@users.noreply.github.com>
2025-11-18 13:01:05 -0500

    Refactor RAM module for cleaner syntax and structure

--- RAM.v
+++ RAM.v
@@ -22,32 +22,30 @@
 module RAM (
     input clk, rst, w,
     input [7:0] data_in,
-    input [(adr_w-1) : 0] data_adr,  // 8 locations
-    output reg [(data_w-1) : 0] data_out
+    input [2:0] data_adr,
+    output reg [7:0] data_out
 );
-    
-parameter data_w = 8;  // 8-bit data width
-parameter adr_w = 3;  // 2^3 = 8 locations
-parameter size = 8;  // # of memory locations = 8
-integer i;
 
-reg [(data_w-1) : 0] mem [(size-1) : 0];  // 8x8 RAM: 8 locations, 8 bits each
+    parameter data_w = 8;
+    parameter adr_w = 3;
+    parameter size = 8;
+    integer i;
 
-always @(posedge clk) begin
-    if (rst) begin
-        // Reset all 8 memory locations to 0
-        for (i = 0; i < size; i = i + 1) begin
-            mem[i] <= {data_w{1'b0}};
+    reg [data_w-1:0] mem [size-1:0];
+
+    always @(posedge clk) begin
+        if (rst) begin
+            for (i = 0; i < size; i = i + 1) begin
+                mem[i] <= {data_w{1'b0}};
+            end
+            data_out <= {data_w{1'b0}};
         end
-        data_out <= {data_w{1'b0}};
-    end
-    else begin
-        if (w) begin  // Write to RAM operation
-            mem[data_adr] <= data_in;
-        end
-        else begin  // Read from RAM operation
-            data_out <= mem[data_adr];
+        else begin
+            if (w)
+                mem[data_adr] <= data_in;
+            else
+                data_out <= mem[data_adr];
         end
     end
-end
+
 endmodule
